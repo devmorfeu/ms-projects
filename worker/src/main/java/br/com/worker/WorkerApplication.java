d3e335637c72656af79e06602ce57e20ae7b5869
morfeu9 <gabriel.nogueira21@hotmail.com>
2021-05-02 13:11:07 -0300

    implementado eureka client

--- worker/src/main/java/br/com/worker/WorkerApplication.java
+++ worker/src/main/java/br/com/worker/WorkerApplication.java
@@ -2,7 +2,9 @@ package br.com.worker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class WorkerApplication {
 
