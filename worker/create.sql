256a3534a4e596758f41c2c6dd4194e701bd203a
morfeu9 <gabriel.nogueira21@hotmail.com>
2021-05-09 18:02:55 -0300

    implementado postgressql no projeto worker

---
+++ worker/create.sql
@@ -0,0 +1,1 @@
+create table tb_worker (id  bigserial not null, daily_income numeric(19, 2), name varchar(255), primary key (id));
--- worker/src/main/java/br/com/worker/WorkerApplication.java
+++ worker/src/main/java/br/com/worker/WorkerApplication.java
@@ -1,7 +1,9 @@
 package br.com.worker;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
